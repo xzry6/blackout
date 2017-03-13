830b786a9b89b3e1063877ba654265583ef8a3a1
xzry6 <xzry6@mail.missouri.edu>
2017-03-13 16:04:53 +0800

    add some documentation

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-    entry: './src/index.tsx',
+    entry: './src/app.tsx',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build'),
